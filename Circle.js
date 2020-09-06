ac322a714b6ae230575598db02f8e63694a7b6ef
VSMent <vsmentvs@gmail.com>
2020-09-06 21:49:39 +0300

    Fix circle eating each other.

--- Circle.js
+++ Circle.js
@@ -1,142 +1,141 @@
 class Circle {
-  radius = 25;
-  repelRadius = this.radius + 15;
-  strokeWidth = 3;
-  repelSpeed = 1;
-  attractRadius = 0;
-  bodyColor = color(255, 204, 0);
-  repelColor = color(251, 202, 251, 170);
-  attractColor = color(202, 251, 222, 100);
-  bodyHoverColor = color(204, 163, 0);
-  isDragged = false;
-  static minX = 0;
-  static maxX = 100;
-  static minY = 0;
-  static maxY = 100;
-  static circles = []
-  static mainCircle;
+    radius = 25;
+    repelRadius = this.radius + 15;
+    strokeWidth = 3;
+    repelSpeed = 1;
+    attractRadius = 0;
+    bodyColor = color(255, 204, 0);
+    repelColor = color(251, 202, 251, 170);
+    attractColor = color(202, 251, 222, 100);
+    bodyHoverColor = color(204, 163, 0);
+    static currentlyDragging = null;
+    static minX = 0;
+    static maxX = 100;
+    static minY = 0;
+    static maxY = 100;
+    static circles = []
+    static mainCircle;
 
-  constructor(x, y, isMain = false) {
-    this.pos = new p5.Vector(x, y);
-    this.isMain = isMain;
-    if (isMain) {
-      this.radius *= 1.5;
-      this.repelRadius *= 3;
-      this.repelSpeed = 10;
-      this.attractRadius = 3 * this.repelRadius;
-    }
-    this.fullRadius = this.radius + this.strokeWidth;
-    Circle.circles.push(this);
-  }
-
-  updatePositions() {
-    if (!this.isDragged) {
-      this.stayInBounds();
-    }
-    this.repelNeighbours();
-    if (this === Circle.mainCircle) {
-      this.attractNeighbours();
-    }
-  }
-
-  stayInBounds() {
-    let dx = 0, dy = 0;
-
-    // X <, >
-    if (this.pos.x - this.fullRadius < Circle.minX) {
-      dx = p5.Vector.sub(this.pos, new p5.Vector(Circle.minX + this.fullRadius, this.pos.y)).normalize().mult(-this.repelSpeed).x;
-    }
-    if (this.pos.x + this.fullRadius > Circle.maxX) {
-      dx = p5.Vector.sub(this.pos, new p5.Vector(Circle.maxX - this.fullRadius, this.pos.y)).normalize().mult(-this.repelSpeed).x;
-    }
-    // Y -, _
-    if (this.pos.y - this.fullRadius < Circle.minY) {
-      dy = p5.Vector.sub(this.pos, new p5.Vector(this.pos.x, Circle.minY + this.fullRadius)).normalize().mult(-this.repelSpeed).y;
-    }
-    if (this.pos.y + this.fullRadius > Circle.maxY) {
-      dy = p5.Vector.sub(this.pos, new p5.Vector(this.pos.x, Circle.maxY - this.fullRadius)).normalize().mult(-this.repelSpeed).y;
-    }
-    this.pos.add(new p5.Vector(dx, dy));
-
-  }
-
-  repelNeighbours() {
-    for (let circle of Circle.circles) {
-      if (circle !== this && circle !== Circle.mainCircle && !circle.isDragged) {
-        let distance = int(this.pos.dist(circle.pos) - this.fullRadius);
-        if (distance < this.repelRadius) {
-          let direction = p5.Vector.sub(this.pos, circle.pos).normalize();
-          circle.pos.add(direction.mult(-this.repelSpeed));
+    constructor(x, y, isMain = false) {
+        this.pos = new p5.Vector(x, y);
+        this.isMain = isMain;
+        if (isMain) {
+            this.radius *= 1.5;
+            this.repelRadius *= 3;
+            this.repelSpeed = 10;
+            this.attractRadius = 3 * this.repelRadius;
         }
-      }
+        this.fullRadius = this.radius + this.strokeWidth;
+        Circle.circles.push(this);
     }
-  }
 
-  attractNeighbours() {
-    for (let circle of Circle.circles) {
-      if (circle !== this && circle !== Circle.mainCircle && !circle.isDragged) {
-        let distance = int(this.pos.dist(circle.pos) + this.fullRadius);
-        if (distance > this.attractRadius) {
-          let direction = p5.Vector.sub(this.pos, circle.pos).normalize();
-          circle.pos.add(direction.mult(this.repelSpeed));
+    updatePositions() {
+        if (Circle.currentlyDragging !== this) {
+            this.stayInBounds();
+        } else {
+            this.drag();
         }
-      }
+        this.repelNeighbours();
+        if (this === Circle.mainCircle) {
+            this.attractNeighbours();
+        }
     }
-  }
 
-  drawLine() {
-    push();
-    stroke(192, 192, 192);
-    strokeWeight(1);
-    // draw line
-    this !== Circle.mainCircle && line(this.pos.x, this.pos.y, Circle.mainCircle.pos.x, Circle.mainCircle.pos.y);
-    pop();
-  }
+    stayInBounds() {
+        let dx = 0, dy = 0;
 
-  draw(i) {
-    push();
-    if (IS_DEBUG) {
-      //draw attract radius
-      strokeWeight(1);
-      fill(this.attractColor);
-      ellipse(this.pos.x, this.pos.y, this.attractRadius * 2);
-      //draw repel radius
-      strokeWeight(1);
-      fill(this.repelColor);
-      ellipse(this.pos.x, this.pos.y, this.repelRadius * 2);
+        // X <, >
+        if (this.pos.x - this.fullRadius < Circle.minX) {
+            dx = p5.Vector.sub(this.pos, new p5.Vector(Circle.minX + this.fullRadius, this.pos.y)).normalize().mult(-this.repelSpeed).x;
+        }
+        if (this.pos.x + this.fullRadius > Circle.maxX) {
+            dx = p5.Vector.sub(this.pos, new p5.Vector(Circle.maxX - this.fullRadius, this.pos.y)).normalize().mult(-this.repelSpeed).x;
+        }
+        // Y -, _
+        if (this.pos.y - this.fullRadius < Circle.minY) {
+            dy = p5.Vector.sub(this.pos, new p5.Vector(this.pos.x, Circle.minY + this.fullRadius)).normalize().mult(-this.repelSpeed).y;
+        }
+        if (this.pos.y + this.fullRadius > Circle.maxY) {
+            dy = p5.Vector.sub(this.pos, new p5.Vector(this.pos.x, Circle.maxY - this.fullRadius)).normalize().mult(-this.repelSpeed).y;
+        }
+        this.pos.add(new p5.Vector(dx, dy));
+
     }
-    //draw circle
-    stroke(0);
-    strokeWeight(this.strokeWidth);
-    this.isHovered() ? fill(this.bodyHoverColor) : fill(this.bodyColor);
-    ellipse(this.pos.x, this.pos.y, this.radius * 2);
-    //draw text
-    textSize(this.baseRadius);
-    textAlign(CENTER, CENTER);
-    noStroke();
-    fill(0);
-    text(i, this.pos.x, this.pos.y);
-    pop();
-  }
 
-  isHovered() {
-    return dist(this.pos.x, this.pos.y, mouseX, mouseY) < this.fullRadius;
-  }
-
-  drag() {
-    if (this.isHovered()) {
-      this.pos = new p5.Vector(mouseX, mouseY);
-      this.isDragged = true;
+    repelNeighbours() {
+        for (let circle of Circle.circles) {
+            if (circle !== this && circle !== Circle.mainCircle && Circle.currentlyDragging !== circle) {
+                let distance = int(this.pos.dist(circle.pos) - this.fullRadius);
+                if (distance < this.repelRadius) {
+                    let direction = p5.Vector.sub(this.pos, circle.pos).normalize();
+                    circle.pos.add(direction.mult(-this.repelSpeed));
+                }
+            }
+        }
     }
-  }
 
-  static drawForceBounds() {
-    push();
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    noFill();
-    rectMode(CORNERS);
-    rect(Circle.minX, Circle.minY, Circle.maxX, Circle.maxY);
-    pop();
-  }
+    attractNeighbours() {
+        for (let circle of Circle.circles) {
+            if (circle !== this && circle !== Circle.mainCircle && Circle.currentlyDragging !== circle) {
+                let distance = int(this.pos.dist(circle.pos) + this.fullRadius);
+                if (distance > this.attractRadius) {
+                    let direction = p5.Vector.sub(this.pos, circle.pos).normalize();
+                    circle.pos.add(direction.mult(this.repelSpeed));
+                }
+            }
+        }
+    }
+
+    drawLine() {
+        push();
+        stroke(192, 192, 192);
+        strokeWeight(1);
+        // draw line
+        this !== Circle.mainCircle && line(this.pos.x, this.pos.y, Circle.mainCircle.pos.x, Circle.mainCircle.pos.y);
+        pop();
+    }
+
+    draw(i) {
+        push();
+        if (IS_DEBUG) {
+            //draw attract radius
+            strokeWeight(1);
+            fill(this.attractColor);
+            ellipse(this.pos.x, this.pos.y, this.attractRadius * 2);
+            //draw repel radius
+            strokeWeight(1);
+            fill(this.repelColor);
+            ellipse(this.pos.x, this.pos.y, this.repelRadius * 2);
+        }
+        //draw circle
+        stroke(0);
+        strokeWeight(this.strokeWidth);
+        this.isHovered() ? fill(this.bodyHoverColor) : fill(this.bodyColor);
+        ellipse(this.pos.x, this.pos.y, this.radius * 2);
+        //draw text
+        textSize(this.baseRadius);
+        textAlign(CENTER, CENTER);
+        noStroke();
+        fill(0);
+        text(i, this.pos.x, this.pos.y);
+        pop();
+    }
+
+    isHovered() {
+        return dist(this.pos.x, this.pos.y, mouseX, mouseY) < this.fullRadius;
+    }
+
+    drag() {
+        this.pos = new p5.Vector(mouseX, mouseY);
+    }
+
+    static drawForceBounds() {
+        push();
+        stroke(255, 0, 0);
+        strokeWeight(1);
+        noFill();
+        rectMode(CORNERS);
+        rect(Circle.minX, Circle.minY, Circle.maxX, Circle.maxY);
+        pop();
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,86 +1,87 @@
 function setup() {
-  createCanvas(800, 700);
-  init();
-  frameRate(30);
-  console.log(circles);
+    createCanvas(800, 700);
+    init();
+    frameRate(30);
+    console.log(circles);
 
-  // noLoop();
+    // noLoop();
 }
 
 function draw() {
-  background(255);
-  if (IS_DEBUG) {
-    drawGrid();
-    Circle.drawForceBounds();
-  }
-  drawConnections();
-  drawCircles();
-  updatePositions();
+    background(255);
+    if (IS_DEBUG) {
+        drawGrid();
+        Circle.drawForceBounds();
+    }
+    drawConnections();
+    drawCircles();
+    updatePositions();
 }
 
 let circles;
 
 function init() {
-  let bounds = 10;  // offset from canvas border
-  Circle.mainCircle = new Circle(width / 2, height / 2, true);
+    let bounds = 10;  // offset from canvas border
+    Circle.mainCircle = new Circle(width / 2, height / 2, true);
 
-  // Create secondary circles
-  for (let i = 0; i < 30; i++) {
-    new Circle(Math.floor((Math.random() * (width - bounds * 2)) + bounds), Math.floor((Math.random() * (height - bounds * 2)) + bounds));
-  }
-  circles = Circle.circles;
+    // Create secondary circles
+    for (let i = 0; i < 30; i++) {
+        new Circle(Math.floor((Math.random() * (width - bounds * 2)) + bounds), Math.floor((Math.random() * (height - bounds * 2)) + bounds));
+    }
+    circles = Circle.circles;
 
-  // Set circle bounds
-  Circle.minX = bounds;
-  Circle.minY = bounds;
-  Circle.maxX = width - bounds;
-  Circle.maxY = height - bounds;
+    // Set circle bounds
+    Circle.minX = bounds;
+    Circle.minY = bounds;
+    Circle.maxX = width - bounds;
+    Circle.maxY = height - bounds;
 }
 
 function updatePositions() {
-  for (let i = 0; i < circles.length; i++) {
-    circles[i].updatePositions(i);
-  }
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].updatePositions(i);
+    }
 }
 
 function drawConnections() {
-  for (let i = 0; i < circles.length; i++) {
-    circles[i].drawLine();
-  }
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].drawLine();
+    }
 }
 
 function drawCircles() {
-  for (let i = 0; i < circles.length; i++) {
-    circles[i].draw(i);
-  }
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].draw(i);
+    }
 }
 
 function drawGrid(step = 10) {
-  push();
-  stroke(192, 192, 192, 100);
-  for (let i = step; i < width; i += step) {
-    line(i, 0, i, height);
-  }
-  for (let i = step; i < height; i += step) {
-    line(0, i, width, i);
-  }
-  pop();
+    push();
+    stroke(192, 192, 192, 100);
+    for (let i = step; i < width; i += step) {
+        line(i, 0, i, height);
+    }
+    for (let i = step; i < height; i += step) {
+        line(0, i, width, i);
+    }
+    pop();
 }
 
 function mouseDragged(event) {
-  if (mouseButton === LEFT) {
-    for (let i = 0; i < circles.length; i++) {
-      circles[i].drag();
+    if (mouseButton === LEFT && !Circle.currentlyDragging) {
+        for (let i = 0; i < circles.length; i++) {
+            if (circles[i].isHovered()) {
+                Circle.currentlyDragging = circles[i];
+                break;
+            }
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 function mouseReleased(event) {
-  if (mouseButton === LEFT) {
-    for (let i = 0; i < circles.length; i++) {
-      circles[i].isDragged = false;
+    if (mouseButton === LEFT) {
+        Circle.currentlyDragging = null;
     }
-  }
-  return false;
+    return false;
 }
